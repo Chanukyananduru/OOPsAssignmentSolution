ea38e705e9b4e7f96030feaf5178bb691a5df427
Chanukyananduru <chanukya.nanduru@gmail.com>
2022-03-20 19:14:28 +0530

    Update Main.java
    Added comments to Main.java

--- department/src/com/driver/Main.java
+++ department/src/com/driver/Main.java
@@ -8,16 +8,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
+		//Creating the objects for each department
 		adminDepartment admin = new adminDepartment();
 		hrDepartment hr = new hrDepartment();
 		techDepartment tech = new techDepartment();
 		
+		//Calling methods from Admin department 
 		System.out.println("Welcome to " + admin.departmentName());
 		System.out.println(admin.getTodaysWork());
 		System.out.println(admin.getWorkDeadline());
 		System.out.println(admin.isTodayAHoliday());
 		System.out.println();
 		
+		//Calling methods from HR department
 		System.out.println("Welcome to " + hr.departmentName());
 		System.out.println(hr.doActivity());	
 		System.out.println(hr.getTodaysWork());
@@ -25,6 +29,8 @@ public class Main {
 		System.out.println(hr.isTodayAHoliday());
 		System.out.println();
 		
+		
+		//Calling methods from Tech department
 		System.out.println("Welcome to "+tech.departmentName());
 		System.out.println(tech.getTodaysWork());
 		System.out.println(tech.getWorkDeadline());
